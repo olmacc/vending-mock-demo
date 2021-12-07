c5fdc56f83d7dce77300e904f5436739b8c6553a
Mau, Oliver <oliver.mau.extern@itergo.com>
2021-12-07 18:41:23 +0100

    initial commit

---
+++ src/main/java/com/example/vendingmockdemo/VendingMockDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vendingmockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMockDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendingMockDemoApplication.class, args);
+    }
+
+}
